cd2cf7a108297fa76937cd81aca345a447d68f66
flyhighair <flyhighup.air25@gmail.com>
2020-03-24 20:42:44 +0900

    Update prettier and eslint setting for vscode

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,10 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+  extends: ['plugin:vue/essential', '@vue/prettier'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'prettier/prettier': [
-      'error',
-      {
-        semi: true,
-        singleQuote: true,
-        trailintComma: 'es5',
-      },
-    ],
   },
   parserOptions: {
     parser: 'babel-eslint',
