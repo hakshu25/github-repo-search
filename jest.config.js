bb40a31f9ea8609fffd79ef41480d9cae9b74613
hakshu <flyhighup.air25@gmail.com>
2020-03-26 08:22:51 +0900

    Add coverage setting

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   setupFiles: ['./tests/setup.js'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,vue}', '!src/main.js', '!src/App.vue'],
+  coverageReporters: ['html', 'text'],
 };
