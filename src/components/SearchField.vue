1bbde1ddc5cc6ef10c157c9cfd9c1e35bc6a6dee
hakshu <flyhighup.air25@gmail.com>
2021-02-23 06:45:05 +0900

    ヘッダー部のスタイルをtailwindで調整

--- src/components/SearchField.vue
+++ src/components/SearchField.vue
@@ -1,18 +1,25 @@
 <template>
-  <div>
-    <input placeholder="Search..." type="text" v-model="searchStr" />
-    <p class="control">
+  <div class="flex flex-wrap">
+    <div class="flex-grow">
+      <input
+        placeholder="Search..."
+        type="text"
+        v-model="searchStr"
+        class="border-gray-100 border-2 rounded h-10 w-full p-2"
+      />
+    </div>
+    <div class="flex-grow-0 ml-4">
       <button
         id="search-btn"
         v-bind:class="{ 'is-loading': isLoading }"
-        class="button is-info"
+        class="h-10 bg-blue-400 text-white py-2 px-6 rounded disabled:opacity-50"
         type="submit"
         v-bind:disabled="!searchStr || isLoading"
         v-on:click="searchRepo"
       >
         Search
       </button>
-    </p>
+    </div>
   </div>
 </template>
 <script>
