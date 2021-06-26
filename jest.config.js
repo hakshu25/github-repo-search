ac864d9f5f4854af46309e51bbcea940a39cbdab
hakshu <flyhighup.air25@gmail.com>
2021-06-26 15:40:42 +0900

    buefyは使わなくなったためテスト用のsetupファイルを削除

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
-  setupFiles: ['./tests/setup.js'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{js,vue}', '!src/main.js', '!src/App.vue'],
   coverageReporters: ['html', 'text'],
