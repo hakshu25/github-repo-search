23e4e700faef52e568a9c702f4d481f780a9f5e7
hakshu <hakshu25@gmail.com>
2023-08-06 15:00:53 +0900

    build: tailwindcssのpurgeオプションをcontentに変更
    ref: https://tailwindcss.com/docs/upgrade-guide#configure-content-sources

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
